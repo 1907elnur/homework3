ba2b5118c76f25176cee39446866eeaeedc14caa
Elnur Gafarzade <138590291+1907elnur@users.noreply.github.com>
2023-07-18 10:20:53 +0400

    Homework-tasks

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Faiz deyerinizi yazin");
+string faiz = Console.ReadLine();
+
+int[] num = { 5, 10, 15 };
+for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] < 20)
+    {
+
+    }
+}
+
